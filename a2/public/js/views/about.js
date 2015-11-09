a9b1bbebe1288b7d1ce4259f807126a421c7a35b
Minty Zhang <minty.mz@gmail.com>
2015-11-08 19:31:46 -0500

    - Adjust dir structure - A2 step 1.2

--- a2/public/js/views/about.js
+++ a2/public/js/views/about.js
@@ -1,16 +1,17 @@
+// catch simple errors
 "use strict";
 
+// declare splat-app namespace if it doesn't already exist
 var splat =  splat || {};
 
+// note View-name (About) matches name of template file Home.html
 splat.About = Backbone.View.extend({
 
-    initialize: function () {
-	this.render();
-    },
-
+    // render the View
     render: function () {
-	this.$el.html(this.template());
-	return this;
+        // set the view element ($el) HTML content using its template
+        this.$el.html(this.template());
+        return this;    // support method chaining
     }
 
 });
--- a2/public/js/views/home.js
+++ a2/public/js/views/home.js
@@ -1,13 +1,17 @@
+// catch simple errors
 "use strict";
 
+// declare splat-app namespace if it doesn't already exist
 var splat =  splat || {};
 
-// note View-name (Home) matches name of template Home.html
+// note View-name (Home) matches name of template file Home.html
 splat.Home = Backbone.View.extend({
 
+    // render the View
     render: function () {
-	this.$el.html(this.template());  // create DOM content for Home
-	return this;    // support chaining
+	// set the view element ($el) HTML content using its template
+	this.$el.html(this.template());
+	return this;    // support method chaining
     }
 
 });

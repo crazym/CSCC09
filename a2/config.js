684955d9e7ff05b6e1d1544541dfdeab61b57666
Miranda Xu <miranda.xuman@gmail.com>
2015-11-16 18:40:26 -0500

    Use MongoLab as db server

--- a2/config.js
+++ a2/config.js
@@ -1,9 +1,10 @@
 // Uses Node module.exports to return object from require()
 module.exports = {
     port: 41428, // replace with your port #
-    dbuser: "zhangmao",
-    dbpass: "tientienlol",
-    dbname: "zhangmao",
+    db: 'mongodb://<MirandaX>:<mdbslowly222>@ds055574.mongolab.com:55574/splat',
+    //dbuser: "zhangmao",
+    //dbpass: "tientienlol",
+    //dbname: "zhangmao",
     env: 'default' // 'default', 'short', 'tiny', 'dev'
     // TODO other fields ADD CODE
 };

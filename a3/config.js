813a5f831663639207c9f5c158c7dac795850584
Minty Zhang <minty.mz@gmail.com>
2015-11-29 02:13:00 -0500

    A3 starter code with config.js and package.json updated:
    set random session secret
    replaced symlink js and css lib with actual libs
    fixed syntax error in style.css...

---
+++ a3/config.js
@@ -0,0 +1,13 @@
+// Uses Node module.exports to return object from require()
+module.exports = {
+    port: 41428, // replace with your port #
+    dbuser: "zhangmao",
+    dbpass: "tientienlol",
+    dbname: "zhangmao",
+    env: 'short', // 'default', 'short', 'tiny', 'dev'
+    // TODO other fields ADD CODE
+    basicAuthUser: "minty",
+    basicAuthPass: "I<3C09",
+    sessionSecret: "topsecret",
+    sessionTimeout: 500,
+};
